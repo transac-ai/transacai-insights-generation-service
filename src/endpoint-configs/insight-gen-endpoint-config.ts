4c77875de0f3efedfc314e0e6d8a71346099f34e
pranav-kural <pranavkural042@gmail.com>
2024-10-25 17:24:56 -0400

    Configuration for insight generation endpoint

---
+++ src/endpoint-configs/insight-gen-endpoint-config.ts
@@ -0,0 +1,7 @@
+import { ChatEndpointConfig } from "@oconva/qvikchat";
+import { getInsightGenSystemPrompt } from "../prompts/insight-gen-prompts";
+
+export const insightGenEndpointConfig: ChatEndpointConfig = {
+  endpoint: "/insight-gen",
+  systemPrompt: getInsightGenSystemPrompt(),
+};
