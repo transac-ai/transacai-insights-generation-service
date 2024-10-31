d8b08c662a1d65c3b2ca538c217d5d00108ac0e0
pranav-kural <pranavkural042@gmail.com>
2024-10-31 18:44:14 -0400

    Added winston logging utility

---
+++ src/utils/logger.ts
@@ -0,0 +1,35 @@
+import { createLogger, format, transports } from "winston";
+
+// Create a logger
+const logger = createLogger({
+  level: "info",
+  defaultMeta: { service: "transac-ai-igs" },
+  format: format.combine(
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level}]: ${message}`;
+    })
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "combined.log" }),
+    //
+    // - Write all logs with importance level of `error` or higher to `error.log`
+    //   (i.e., error, fatal, but not other levels)
+    //
+    new transports.File({ filename: "error.log", level: "error" }),
+  ],
+});
+
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new transports.Console({
+      format: format.simple(),
+    })
+  );
+}
+
+export default logger;
